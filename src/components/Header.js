c561827cba75c580d3a54884e750ed4ab77a7190
Trevor <trevorsitu8@gmail.com>
2024-01-07 06:22:37 -0800

    added Button as a Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
   return (
-    <header>
+    <header className='header'>
         <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
+} 
+
+
+Button.defaultProps = {
+    color: 'steelblue'
 }
 
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
+}
 Header.defaultProps = {
     title: "Task Tracker",
 }
